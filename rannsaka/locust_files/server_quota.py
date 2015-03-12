973d351b1d873889345fcb9b44a846fc66fbe7ef
pcrews <gleebix@gmail.com>
2015-03-12 13:56:57 -0700

    Tweaking test case to only do create/delete + server_list* operations

--- rannsaka/locust_files/server_quota.py
+++ rannsaka/locust_files/server_quota.py
@@ -21,18 +21,18 @@ class NovaSnapshot(baseTaskSet):
         self.server_count = 0
         self.image_count = 0
         self.min_server_count = 10 
-        self.max_server_count = 9 
+        self.max_server_count = 10
         # limit list to less core / disk-intensive flavors in general
         self.flavor_list = [42,42,84,84,451]
 
 
     tasks = {nova_util.churn_server_pool: 5,
-             nova_base.list_images: 4,
-             nova_base.list_image_detail: 1,
-             nova_base.list_image_metadata: 1,
-             nova_util.create_server_image: 1,
-             nova_base.reboot_server: 7,
-             nova_util.resize_server: 7,
+             nova_base.list_servers: 4,
+             nova_base.list_servers_detail: 2,
+             nova_base.list_server_detail: 1,
+             #nova_util.create_server_image: 1,
+             #nova_base.reboot_server: 7,
+             #nova_util.resize_server: 7,
              nova_util.refresh_auth_token: 1
             } 
 
